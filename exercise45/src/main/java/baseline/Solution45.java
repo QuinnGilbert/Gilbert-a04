60d55bc9da5ac72368e1c9cc8c6035ef5617780a
QuinnG <quinngilbert12and3@gmail.com>
2021-10-17 22:29:10 -0400

    45 code

--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -9,12 +9,31 @@ import java.io.*;
 public class Solution45 {
     public static void main(String[] args) throws Exception{
         //input string
+        System.out.println("CHECK1");
+        Scanner in = new Scanner(new FileReader("data/exercise45_input.txt"));
+        String data = "";
+        System.out.println("CHECK");
+        while(in.hasNext()){
+            data+=in.nextLine()+"\n";
+        }
+        System.out.println(data);
         //replace occurrences [replace()]
+        String output = replace(data);
         //write result to file
+        PrintWriter out = new PrintWriter(new FileWriter("data/newFile.txt"));
     }
 
     static String replace(String data){
-        //loop through all occurences and replace add different value to string
-
+        //loop through all occurrences and replace add different value to string
+        String result = "";
+        for(int i=0;i<data.length();i++){
+            if(!data.substring(i,i+7).equals("utilize")){
+                result+="use";
+                i+=7;
+            }else{
+                result+=data.charAt(i);
+            }
+        }
+        return result;
     }
 }
