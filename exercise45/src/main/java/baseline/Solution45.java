f83fbb14b1249050f8c8ed36324fe82bd0ba613a
QuinnG <quinngilbert12and3@gmail.com>
2021-10-17 22:16:09 -0400

    45 skeleton

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import java.util.*;
+import java.io.*;
+
+public class Solution45 {
+    public static void main(String[] args) throws Exception{
+        //input string
+        //replace occurrences [replace()]
+        //write result to file
+    }
+
+    static String replace(String data){
+        //loop through all occurences and replace add different value to string
+
+    }
+}
